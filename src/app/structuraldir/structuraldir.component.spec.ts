ddd6e163b96f0d0d3209b9b8b5923583082990de
Sachin Pandey <s.pandey1@zensar.com>
2022-05-02 20:47:50 +0530

    this is commit

---
+++ src/app/structuraldir/structuraldir.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StructuraldirComponent } from './structuraldir.component';
+
+describe('StructuraldirComponent', () => {
+  let component: StructuraldirComponent;
+  let fixture: ComponentFixture<StructuraldirComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StructuraldirComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StructuraldirComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
